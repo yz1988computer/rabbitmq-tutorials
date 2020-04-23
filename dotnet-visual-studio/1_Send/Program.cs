407efda6fe491ef75b5413046933f0d1e7c64782
yz <yz1988computer@163.com>
2020-04-23 20:35:25 +0800

    format data

--- dotnet-visual-studio/1_Send/Program.cs
+++ dotnet-visual-studio/1_Send/Program.cs
@@ -11,7 +11,6 @@ class Program
         using(var channel = connection.CreateModel())
         {
             channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
-
             string message = "Hello World!";
             var body = Encoding.UTF8.GetBytes(message);
 
